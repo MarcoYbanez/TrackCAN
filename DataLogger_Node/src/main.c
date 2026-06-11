0eb09da9d17d99eb6e308677456da2fbda095665
Marco Ybanez <openinviteapplication@gmail.com>
2026-06-11 12:58:15 -0700

    Zephyr random number generator application

---
+++ DataLogger_Node/src/main.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/entropy.h>
+
+// #define RAND_GEN DT_ALIAS(randdd)
+// const struct device *random_gen = DEVICE_DT_GET(RAND_GEN);
+
+const struct device *random_gen = DEVICE_DT_GET(DT_CHOSEN(zephyr_entropy));
+int main(void)
+{
+    while (1) {
+        uint32_t x = 100;
+        int ret = entropy_get_entropy(random_gen, (uint8_t *)&x, sizeof(x));
+
+				printf("true random number: %u\n",
+       (uint32_t)x);
+        k_msleep(500);
+    }
+
+    return 0;
+}
