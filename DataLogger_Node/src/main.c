1835c35075a14572813495502386b62efbaba854
Marco Ybanez <openinviteapplication@gmail.com>
2026-06-11 19:06:29 -0700

    Fix issue with overlay and random number printing

--- DataLogger_Node/src/main.c
+++ DataLogger_Node/src/main.c
@@ -4,18 +4,23 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/entropy.h>
 
-// #define RAND_GEN DT_ALIAS(randdd)
-// const struct device *random_gen = DEVICE_DT_GET(RAND_GEN);
+#define RAND_GEN DT_ALIAS(randdd)
+#if !DT_NODE_HAS_STATUS(RAND_GEN, okay)
 
-const struct device *random_gen = DEVICE_DT_GET(DT_CHOSEN(zephyr_entropy));
+#error "randdd alias does not point to an okay device"
+
+#endif
+
+const struct device *random_gen = DEVICE_DT_GET(RAND_GEN);
+// const struct device *random_gen = DEVICE_DT_GET(DT_CHOSEN(zephyr_entropy));
 int main(void)
 {
     while (1) {
         uint32_t x = 100;
         int ret = entropy_get_entropy(random_gen, (uint8_t *)&x, sizeof(x));
+        // int ret = entropy_get_entropy(random_gen, (uint8_t *)&x, sizeof(x));
 
-				printf("true random number: %u\n",
-       (uint32_t)x);
+        printf("true random number: %u\n", (uint32_t)x);
         k_msleep(500);
     }
 
